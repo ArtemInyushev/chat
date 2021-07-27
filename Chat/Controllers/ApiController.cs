c19d14a7a97177874732a13755ba1d90ee813e4e
ArtemInyushev <art10inyushev@gmail.com>
2021-07-27 21:13:17 +0300

    create project

---
+++ Chat/Controllers/ApiController.cs
@@ -0,0 +1,23 @@
+﻿using Chat.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.Controllers {
+    [ApiController]
+    public class ApiController : Controller {
+        private ApplicationContext db;
+        public ApiController(ApplicationContext context) {
+            db = context;
+        }
+        [HttpGet("")]
+        public async Task<ActionResult> Index() {
+            //List<User> users = await db.Users.ToListAsync();
+            return await Task.Run(() => View());
+        }
+    }
+}
---
+++ Chat/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.Models {
+    public class User {
+        public Int64 Id { get; set; }
+        public string Name { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime RegisteredAt { get; set; }
+        public string LogoUrl { get; set; }
+        public string Email { get; set; }
+    }
+}
