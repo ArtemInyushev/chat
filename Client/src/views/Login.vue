59e6cec4fc35ebad72a067385057ad8c1a9c4987
ArtemInyushev <art10inyushev@gmail.com>
2021-08-18 13:54:52 +0300

    day11

---
+++ Client/src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="login">
+        <img src="https://res.cloudinary.com/duzykfess/image/upload/v1627071932/Chat/lock_inmjg5.jpg" class="login" />
+
+        <section class="login_form">
+            <form onsubmit="LoginAction(event)">
+                <h1 class="display-4 centered_text white">Login</h1>
+
+                <input type="text" class="input_form blackground white margin_bottom" placeholder="Username..." maxlength="30" required>
+                <input type="password" class="input_form blackground white margin_bottom" placeholder="Password..." maxlength="30" required>
+
+                <div class="margin_bottom">
+                    <input type="checkbox" class="btn-check" id="remember_me" autocomplete="off">
+                    <label class="btn btn-outline-light" for="remember_me">Remember me</label>
+                    <input type="submit" class="btn btn-outline-light " value="Log in" />
+                </div>
+
+                <a class="link-light" onclick="GoToCreateAccountPage()">Create Account</a>
+            </form>
+        </section>
+    </div>
+</template>
