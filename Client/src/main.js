a2972bebe81bd4441c1e889c2043c36b81359eb6
ArtemInyushev <art10inyushev@gmail.com>
2021-08-27 21:19:06 +0300

    revert vue signalR install

--- Client/src/main.js
+++ Client/src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import VueSignalR from 'vue-signalr'
 
-Vue.use(VueSignalR, 'https:///localhost:44360/chat');
 Vue.config.productionTip = false; 
 
 new Vue({
