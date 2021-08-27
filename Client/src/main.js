9853a107aa2d33a32acf87da16959617556401e6
ArtemInyushev <art10inyushev@gmail.com>
2021-08-27 17:53:34 +0300

    vue signalR install

--- Client/src/main.js
+++ Client/src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue';
 //import Vuex from 'vuex'
 import App from './App.vue';
 import router from './router';
+import VueSignalR from 'vue-signalr'
 
 //Vue.use(Vuex);
+Vue.use(VueSignalR, 'SOCKET_URL');
 Vue.config.productionTip = false; 
 
 new Vue({
